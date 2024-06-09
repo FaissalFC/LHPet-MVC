5418518734e0b4af5d4fccc0aa3ca00d3e3d89d2
FaissalFC <142908515+FaissalFC@users.noreply.github.com>
2024-06-09 12:32:15 -0300

    Add files via upload

---
+++ LHPet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LHPet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
